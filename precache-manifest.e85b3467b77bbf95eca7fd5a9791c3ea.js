7d349b91eea5670776cb142908149e7209bedc0c
Shaik-Amreen <amreenshaik40@gmail.com>
2020-11-03 16:06:55 +0530

    Updates

---
+++ precache-manifest.e85b3467b77bbf95eca7fd5a9791c3ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fd6c58767c93a5cf1d726ce0ec18d4",
+    "url": "/teams/index.html"
+  },
+  {
+    "revision": "1179609d6320bd84ed5e",
+    "url": "/teams/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "4a22a2fd5fa74d1c5955",
+    "url": "/teams/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1179609d6320bd84ed5e",
+    "url": "/teams/static/js/2.241b606b.chunk.js"
+  },
+  {
+    "revision": "2f97bfe5b7c1c72b23be28739730a643",
+    "url": "/teams/static/js/2.241b606b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a22a2fd5fa74d1c5955",
+    "url": "/teams/static/js/main.8b58de3f.chunk.js"
+  },
+  {
+    "revision": "90d284be2e00ae6963b9",
+    "url": "/teams/static/js/runtime-main.01b0981a.js"
+  }
+]);
